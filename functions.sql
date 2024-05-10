aac444ae806f7502d9f9acf6ccb3718dcf0544ed
RusVS1 <112542219+RusVS1@users.noreply.github.com>
2024-05-10 17:49:22 +0800

    new indexes and functions

--- functions.sql
+++ functions.sql
@@ -11,7 +11,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT get_paste(131);
+--SELECT get_paste(131);
 --SELECT get_paste(56);
 
 
@@ -51,38 +51,26 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
---CALL bying_lot(133);
+--CALL bying_lot(149);
 
 
---Я не придумал еще одну интересную функцию, поэтому написал удаление базы данных
-CREATE OR REPLACE PROCEDURE drop_database() AS $$
+--Нахождение сета по ключевому начальному слову и его минимальной цены
+CREATE OR REPLACE FUNCTION find_set(keyword VARCHAR(256))
+RETURNS TABLE (item_name VARCHAR(256), min_price INTEGER, lot_id INTEGER) AS $$
 BEGIN
-    EXECUTE 'DROP INDEX IF EXISTS idx_active_lots_seller';
-    EXECUTE 'DROP INDEX IF EXISTS idx_active_lots_item';
-    EXECUTE 'DROP INDEX IF EXISTS idx_lots_history_seller';
-    EXECUTE 'DROP INDEX IF EXISTS idx_lots_history_item';
-    EXECUTE 'DROP INDEX IF EXISTS idx_category';
-
-    EXECUTE 'DROP TABLE IF EXISTS items_categories CASCADE';
-    EXECUTE 'DROP TABLE IF EXISTS resources CASCADE';
-    EXECUTE 'DROP TABLE IF EXISTS items CASCADE';
-    EXECUTE 'DROP TABLE IF EXISTS resources_for_items CASCADE';
-    EXECUTE 'DROP TABLE IF EXISTS sellers CASCADE';
-    EXECUTE 'DROP TABLE IF EXISTS active_lots CASCADE';
-    EXECUTE 'DROP TABLE IF EXISTS lots_history CASCADE';
-
-    EXECUTE 'DROP VIEW IF EXISTS seller_info';
-    EXECUTE 'DROP VIEW IF EXISTS item_creation_time';
-    EXECUTE 'DROP VIEW IF EXISTS item_resources';
-    EXECUTE 'DROP VIEW IF EXISTS priority_active_lots';
-    EXECUTE 'DROP VIEW IF EXISTS orted_lots_history';
-
-
-    EXECUTE 'DROP FUNCTION IF EXISTS get_paste';
-    EXECUTE 'DROP PROCEDURE IF EXISTS bying_lot';
-    EXECUTE 'DROP PROCEDURE IF EXISTS drop_database';
+    RETURN QUERY
+    SELECT items.item_name, MIN(active_lots.price_piece) AS min_price, MIN(active_lots.id) AS lot_id
+    FROM items
+    LEFT JOIN active_lots ON items.item_name = active_lots.item
+    LEFT JOIN sellers ON active_lots.seller = sellers.seller_nickname
+    WHERE items.item_name LIKE keyword || '%'
+      AND sellers.seller_status IN ('Online in game', 'Online')
+    GROUP BY items.item_name;
 END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE 'plpgsql';
 
-CALL drop_database();
+--SELECT * FROM find_set('Saryn');
 
+
+
+--DROP FUNCTION IF EXISTS find_set;
--- indexes.sql
+++ indexes.sql
@@ -1,10 +1,8 @@
-CREATE INDEX idx_active_lots_seller ON active_lots (seller);
-CREATE INDEX idx_active_lots_item ON active_lots (item);
+CREATE INDEX idx_active_lots_seller_date_put ON active_lots (seller, date_put);
+CREATE INDEX idx_active_lots_item_date_put ON active_lots (item, date_put);
 
-CREATE INDEX idx_lots_history_seller ON lots_history (seller);
-CREATE INDEX idx_lots_history_item ON lots_history (item);
-
-CREATE INDEX idx_category ON items (category);
+CREATE INDEX idx_lots_history_seller_sale_date ON lots_history (seller, sale_date);
+CREATE INDEX idx_lots_history_item_sale_date ON lots_history (item, sale_date);
 
 
 --DROP INDEX IF EXISTS idx_category;
